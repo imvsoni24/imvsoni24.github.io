0cbd148d0cc6ebfc0bb6648699a22686981c36e2
imvsoni24 <imvsoni24@gmail.com>
2023-01-03 12:58:30 +0530

    task1

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+let icon = document.getElementById("toggle")
+let img = document.getElementById("myimg");
+let logo = document.getElementById("logo");
+icon.onclick=function(){
+    document.body.classList.toggle("dark-theme")
+    if(document.body.classList.contains("dark-theme")){
+     img.src="https://i.ibb.co/2gbYNQr/My-project-1-3.png"
+     logo.src="https://i.ibb.co/9GQDmPW/V-1.gif"
+    }else{
+        img.src = "https://i.ibb.co/PjnrNb1/My-project-1-2.png"
+        logo.src = "https://i.ibb.co/T0CWy8h/V.gif"
+    }
+}
 
-function myFunction(){
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("app2");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+function my(){
         window.location.href = "https://drive.google.com/uc?id=1Is4NFOIM0cyZHtIuye69FauFuGpzn48X&export=download",
         window.open("https://drive.google.com/file/d/1Is4NFOIM0cyZHtIuye69FauFuGpzn48X/view?usp=share_link")
 }
@@ -16,6 +42,7 @@ document.addEventListener('click', () => {
         cursor.classList.remove("expand");
     }, 500)
 })
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "320px";
   }
